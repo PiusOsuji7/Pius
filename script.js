0221cbb395a4ecb2d00db47c02c053c34be7257e
Pius Osuji <osujipius7@gmail.com>
2021-08-13 21:33:53 +1200

    First deployment

---
+++ script.js
@@ -0,0 +1,27 @@
+//INITIALISE SWIPER
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+//INITIALIZE AOS
+  AOS.init({
+    offset: 300,
+    duration: 600
+  });
+
+ 
+
+
